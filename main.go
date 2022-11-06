0baff3174780c00e2570a0332fa20e26347e3a38
b4cktr4ck5r3 <flennertom@gmail.com>
2022-11-06 18:19:15 +0100

    refactoring

--- main.go
+++ main.go
@@ -5,77 +5,22 @@ import (
 	"os"
 	"os/signal"
 
-	"github.com/b4cktr4ck5r3/rpl-discordbot/commands"
 	"github.com/b4cktr4ck5r3/rpl-discordbot/config"
-	"github.com/bwmarrin/discordgo"
+	"github.com/b4cktr4ck5r3/rpl-discordbot/session"
 )
-
-var (
-	integerOptionMinValue          = 1.0
-	dmPermission                   = false
-	defaultMemberPermissions int64 = discordgo.PermissionManageServer
-)
-
-var s *discordgo.Session
 
 func main() {
 	config.InitializeConfig()
+	session.InitializeSession()
+	session.RegisterCommands()
 
-	var err error
-	s, err = discordgo.New("Bot " + config.Cfg.BotToken)
-
-	if err != nil {
-		log.Fatalf("Invalid bot parameters: %v", err)
-	}
-
-	s.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
-		if h, ok := commands.CommandHandlers[i.ApplicationCommandData().Name]; ok {
-			h(s, i)
-		}
-	})
-
-	s.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
-		log.Printf("Logged in as: %v#%v", s.State.User.Username, s.State.User.Discriminator)
-	})
-
-	err = s.Open()
-
-	if err != nil {
-		log.Fatalf("Cannot open the session: %v", err)
-	}
-
-	log.Println("Adding commands...")
-	registeredCommands := make([]*discordgo.ApplicationCommand, len(commands.Commands))
-	for i, v := range commands.Commands {
-		cmd, err := s.ApplicationCommandCreate(s.State.User.ID, "", v)
-		if err != nil {
-			log.Panicf("Cannot create '%v' command: %v", v.Name, err)
-		}
-		registeredCommands[i] = cmd
-	}
-
-	defer s.Close()
+	defer session.S.Close()
 
 	stop := make(chan os.Signal, 1)
 	signal.Notify(stop, os.Interrupt)
 	log.Println("Press Ctrl+C to exit")
 	<-stop
 
-	log.Println("Removing commands...")
-	// // We need to fetch the commands, since deleting requires the command ID.
-	// // We are doing this from the returned commands on line 375, because using
-	// // this will delete all the commands, which might not be desirable, so we
-	// // are deleting only the commands that we added.
-	// registeredCommands, err := s.ApplicationCommands(s.State.User.ID, *GuildID)
-	// if err != nil {
-	// 	log.Fatalf("Could not fetch registered commands: %v", err)
-	// }
-	for _, v := range registeredCommands {
-		err := s.ApplicationCommandDelete(s.State.User.ID, "", v.ID)
-		if err != nil {
-			log.Panicf("Cannot delete '%v' command: %v", v.Name, err)
-		}
-	}
-
+	session.RemoveCommands()
 	log.Println("Gracefully shutting down.")
 }
---
+++ session/session.go
@@ -0,0 +1,65 @@
+package session
+
+import (
+	"log"
+
+	"github.com/b4cktr4ck5r3/rpl-discordbot/commands"
+	"github.com/b4cktr4ck5r3/rpl-discordbot/config"
+	"github.com/bwmarrin/discordgo"
+)
+
+var S *discordgo.Session
+var RegisteredCommand []*discordgo.ApplicationCommand
+
+func InitializeSession() {
+	var err error
+	S, err = discordgo.New("Bot " + config.Cfg.BotToken)
+
+	err = S.Open()
+
+	if err != nil {
+		log.Fatalf("Cannot open the session: %v", err)
+	}
+
+	//Display message on ready
+	S.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		log.Printf("Logged in as: %v#%v", s.State.User.Username, s.State.User.Discriminator)
+	})
+}
+
+func RegisterCommands() {
+	log.Println("Adding commands...")
+	RegisteredCommand := make([]*discordgo.ApplicationCommand, len(commands.Commands))
+	for i, v := range commands.Commands {
+		cmd, err := S.ApplicationCommandCreate(S.State.User.ID, "", v)
+		if err != nil {
+			log.Panicf("Cannot create '%v' command: %v", v.Name, err)
+		}
+		RegisteredCommand[i] = cmd
+	}
+
+	log.Println("Handling commands...")
+	S.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		if h, ok := commands.CommandHandlers[i.ApplicationCommandData().Name]; ok {
+			h(s, i)
+		}
+	})
+}
+
+func RemoveCommands() {
+	log.Println("Removing commands...")
+	// // We need to fetch the commands, since deleting requires the command ID.
+	// // We are doing this from the returned commands on line 375, because using
+	// // this will delete all the commands, which might not be desirable, so we
+	// // are deleting only the commands that we added.
+	// registeredCommands, err := s.ApplicationCommands(s.State.User.ID, *GuildID)
+	// if err != nil {
+	// 	log.Fatalf("Could not fetch registered commands: %v", err)
+	// }
+	for _, v := range RegisteredCommand {
+		err := S.ApplicationCommandDelete(S.State.User.ID, "", v.ID)
+		if err != nil {
+			log.Panicf("Cannot delete '%v' command: %v", v.Name, err)
+		}
+	}
+}
